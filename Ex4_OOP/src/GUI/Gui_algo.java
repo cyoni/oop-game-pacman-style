e5ac277478103b183ebbc492d235807052c11ccf
Yoni <cyoni10@gmail.com>
2020-04-30 20:23:34 +0300

    fixed some algos

--- Ex4_OOP/src/GUI/Gui_algo.java
+++ Ex4_OOP/src/GUI/Gui_algo.java
@@ -2,17 +2,23 @@ package GUI;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.RenderingHints;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.geom.AffineTransform;
 import java.awt.geom.Line2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map.Entry;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -42,6 +48,7 @@ import game.InitGame;
 import mouse.MouseClickOnScreen;
 
 import javax.imageio.ImageIO;
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 
 /**
@@ -73,8 +80,7 @@ public class Gui_algo extends JPanel  {
     public synchronized void paintComponent(Graphics graphics) {
         super.paintComponent(graphics);
     	graphics.drawImage(images.getBackground(), 0, 0, Screen.WIDTH, Screen.HEIGHT, this); // set background
-   
-    	if (gameboard != null) {
+     	if (gameboard != null) {
 	    	List<Game_object> fruits = gameboard.getFruits();
 	    	draw(graphics, fruits, images.getFruit_image());
 	    	List<Game_object> pacmans = gameboard.getPacmans();
@@ -89,32 +95,78 @@ public class Gui_algo extends JPanel  {
 	    	}
     	}
     }
-
+    public static void drawLabel(Graphics2D g2d, double x, double y, double angle, String text) 
+    {    
+        g2d.translate((float)x,(float)y);
+        g2d.rotate(Math.toRadians(angle));
+        g2d.drawString(text,0,0);
+        g2d.rotate(-Math.toRadians(angle));
+        g2d.translate(-(float)x,-(float)y);
+    }  
     
     public synchronized void paint(Graphics g) {
         super.paint(g); 
+       
+        drawGraph(g);
+	    drawBottomLabel(g);
+
+    }
+
+	private void drawBottomLabel(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
-        
-        if (menu.getIsShow_Game_Graph_Selected()) {
+        Font myFont = new Font("Arial", Font.BOLD, 25);    
+        g2.setColor(Color.green);
+        g2.setFont(myFont);
+        drawLabel(g2, (Screen.WIDTH/2)-100 , Screen.HEIGHT-20 , 0, Screen.getScreenLabel());  
+	}
+
+	private void drawGraph(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        if (GameBoard.show_game_graph) {
         	List<Line> lines = gameboard.getLinesOfGameGraph();
 	        for (int i=0; i < lines.size(); i++) {
 	            Line currentLine = lines.get(i);
+		        drawDistanceLabels(g, currentLine);
 	            Line2D lin = new Line2D.Double(currentLine.getP1().x(), currentLine.getP1().y(), currentLine.getP2().x(), currentLine.getP2().y());
 	            g2.setColor(Color.yellow);
 	            g2.draw(lin);
 	        }
         } 
         
-	    if (menu.getIsShow_MST_Graph_Selected()) {
+	    if (GameBoard.showMSTPath) {
 	    	List<Line> lines = gameboard.getMST_Game();
+        	Screen.resetDistance();
 		    for (int i=0; i<lines.size(); i++) {
 		        Line currentLine = lines.get(i);
+		        drawDistanceLabels(g, currentLine);
 		        Line2D lin = new Line2D.Double(currentLine.getP1().x(), currentLine.getP1().y(), currentLine.getP2().x(), currentLine.getP2().y());
 		        g2.setColor(Color.GREEN);
 		        g2.draw(lin);
 	        }
-	    } 
-    }
+	    }		
+	}
+
+	private void drawDistanceLabels(Graphics g, Line line) {
+        Graphics2D g2 = (Graphics2D) g;
+        Point2D p1 = line.getP1();
+        Point2D p2 = line.getP2();
+        double distance = Line.distance(map.pixel2global(p1), map.pixel2global(p2)); // IMPORTANT: to optimize!!!!!!!!!!!!!!!!
+        distance = Double.parseDouble(new DecimalFormat("##.#").format(distance));
+        double y1 = (int) p1.y();
+        double x1 = (int) p1.x();
+        double y2 = (int) p2.y();
+        double x2 = (int) p2.x();
+        double rad = Math.atan2(y1-y2, x1-x2);
+        double degrees = Math.toDegrees(rad);
+            
+        if (x2 > x1) degrees = 180-degrees;
+        
+        Font myFont = new Font("Arial", Font.BOLD, 11);    
+        g2.setColor(Color.WHITE);
+        g2.setFont(myFont);
+        Screen.sumDistance(distance);
+        drawLabel(g2, (x1+x2)/2, (y1+y2)/2, degrees, distance+"");  
+	}
 
 	private void draw(Graphics graphics, List<Game_object> obj, Image picture) {
 		for (int i=0; i<obj.size(); i++) {
@@ -142,7 +194,6 @@ public class Gui_algo extends JPanel  {
 		frame.setLocationRelativeTo(null);
 	}
 	
-
 	public void setGameBoard(GameBoard gameBoard) {
 		this.gameboard = gameBoard;
 		this.repaint();
@@ -152,20 +203,16 @@ public class Gui_algo extends JPanel  {
 		CSVReaderAndWriter csvWriter = new CSVReaderAndWriter();
 		String file_name = csvWriter.chooseFile();
 		csvWriter.writeToCSV(gameboard, file_name);
-		
 	}
 
 	public void startBackgroundAnimation() {
 		AnimatedBackground animation = new AnimatedBackground(this);
 		animation.start();
-		
 	}
 
 	public void buildGraph() {
 		initGame.buildGraphGame();
 		repaint();
 	}
-
-
 
 }
